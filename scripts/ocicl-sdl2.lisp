c90c8fb960ee98358c17a83cc5de471b5c68a810
Jeff Sparkes <jsparkes@opentext.com>
2024-08-21 07:16:27 -0400

    Simplify configuration of ocicl.

--- scripts/ocicl-sdl2.lisp
+++ scripts/ocicl-sdl2.lisp
@@ -7,23 +7,30 @@
   (when (probe-file quicklisp-init)
     (load quicklisp-init)))
 
+(let* ((dir (if (member :win32 *features*)  "AppData/local/ocicl/" ".local/share/ocicl/"))
+       (file (merge-pathnames (parse-namestring
+		 	       (concatenate 'string
+				 	    (namestring (user-homedir-pathname))
+					    dir "/ocicl-runtime.lisp")))))
+  (when (probe-file file)
+    (load file)
+    (pushnew (uiop:getcwd) asdf:*central-registry* :test 'equalp)))
+
 (ql:quickload :str)
 
-+(and (not (member :ocicl *features*)) (member :win32 *features*))
-(let ((file (merge-pathnames (parse-namestring (str:concat (namestring (user-homedir-pathname)) "AppData/Local/ocicl/")) "ocicl-runtime.lisp")))
-  (when (probe-file file)
-    (load file)
-    (pushnew (uiop:getcwd) asdf:*central-registry* :test 'equalp)))
+;; (princ (asdf:asdf-version))
 
-+(and (not (member :ocicl *features*)) (not (member :win32 *features*)))
-(let ((file (merge-pathnames (parse-namestring (str:concat (namestring (user-homedir-pathname)) ".local/share/ocicl/")) "ocicl-runtime.lisp")))
-  (when (probe-file file)
-    (load file)
-    (pushnew (uiop:getcwd) asdf:*central-registry* :test 'equalp)))
+#+win32
+(setf (uiop:getenv "PATH")
+      (concatenate 'string (uiop:getenv "PATH")
+		   ";" (namestring (uiop:getcwd)) "win64"))
+
+;; (princ (uiop:getenv "PATH"))
 
 #+win32
 (ql:quickload :trivial-package-local-nicknames)
 (ql:quickload :lem-sdl2)
+
 (lem:init-at-build-time)
 
 (sb-ext:save-lisp-and-die "lem"
